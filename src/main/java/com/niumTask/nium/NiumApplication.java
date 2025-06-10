a3c7d07cbc24cf9d8c0c8698b3652b0c935a4d02
alessio <a.ghirardi@almaviva.it>
2025-06-10 13:34:17 +0200

    init project

---
+++ src/main/java/com/niumTask/nium/NiumApplication.java
@@ -0,0 +1,13 @@
+package com.niumTask.nium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niumTask/nium/NiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niumTask.nium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
